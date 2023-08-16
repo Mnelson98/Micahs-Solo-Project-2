f90dc1e90c0f33e149b87bd207c070238c6a72ee
Mnelson98 <138735614+Mnelson98@users.noreply.github.com>
2023-08-16 19:19:39 -0400

    Solo Project V3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 // import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
+//import Axios from "axios"; <<<<--- USED TO CONNECT FRONT AND BACK END
 
 export function App() {
+
+    // const [data, setData] = useState(""); <<<<----- syntax to use axios and make backend requests
+    // const getData = async() => {
+    //     const response = await Axios.get("http://localhost:3010/getData");
+    //     setData(response);
+    // }
+
+    // useEffect(() => {
+    //     getData()
+    // }, []);
+
     return (
         <div className="todo-app">
             <TodoList />
